c116cde2a3da1e019820a282f0b476a6cfb778c2
gonultasbu <gonultasbu@users.noreply.github.com>
2018-05-25 22:50:04 +0300

    confusion matrix generator added

--- total_evaluation.py
+++ total_evaluation.py
@@ -4,6 +4,15 @@ def evaluate_everything(input_dir):
     result_file = open("DETAILED_EVALUATION.eval", 'w')
     sleepy_success_counter = 0.0
     sleepy_success_accumulator = 0.0
+    epsilon=0.00000000001
+    true_positive_accumulator=0.0
+    true_positive_counter=0.0
+    false_positive_accumulator=0.0
+    false_positive_counter=0.0
+    true_negative_accumulator=0.0
+    true_negative_counter=0.0
+    false_negative_accumulator=0.0
+    false_negative_counter=0.0
 
     nonsleepy_success_counter = 0.0
     nonsleepy_success_accumulator = 0.0
@@ -21,20 +30,44 @@ def evaluate_everything(input_dir):
 
             elif (name.endswith("sleepyCombination.eval")):
                 sleepy_success_counter=sleepy_success_counter+1
+                true_negative_counter=true_negative_counter+1
+                true_positive_counter=true_positive_counter+1
+                false_negative_counter=false_negative_counter+1
+                false_positive_counter=false_positive_counter+1
                 s_File = open(os.path.join(root,name), 'r')
                 sleepy_string = s_File.read()
                 sleepy_list=sleepy_string.split(" ")
                 sleepy_success_rate=float(sleepy_list[15])
+                true_positive = float(sleepy_list[3].split("\n")[0])
+                true_negative = float(sleepy_list[6].split("\n")[0])
+                false_positive = float(sleepy_list[9].split("\n")[0])
+                false_negative = float(sleepy_list[12].split("\n")[0])
+                true_positive_rate = true_positive / (true_positive+false_positive+epsilon)
+                true_negative_rate = true_negative / (true_negative+false_negative+epsilon)
+                false_positive_rate = false_positive / (true_positive+false_positive+epsilon)
+                false_negative_rate = false_negative / (true_negative+false_negative+epsilon)
                 s_File.close()
                 sleepy_success_accumulator = sleepy_success_accumulator + sleepy_success_rate
+                true_positive_accumulator = true_positive_accumulator + true_positive_rate
+                true_negative_accumulator = true_negative_accumulator + true_negative_rate
+                false_positive_accumulator = false_positive_accumulator + false_positive_rate
+                false_negative_accumulator = false_negative_accumulator + false_negative_rate
                 result_file.write(str(os.path.join(root, name) + "\n" + str(sleepy_success_rate) + "\n\n" ))
             else:
                 pass
 
-    result_file.write ("TOTAL NONSLEEPY SUCCESS RATE = " + str(nonsleepy_success_accumulator / nonsleepy_success_counter))
-    result_file.write ("\n" + "TOTAL SLEEPY SUCCESS RATE = " + str(sleepy_success_accumulator / sleepy_success_counter))
-    print ("TOTAL NONSLEEPY SUCCESS RATE = " + str(nonsleepy_success_accumulator / nonsleepy_success_counter))
-    print ("TOTAL SLEEPY SUCCESS RATE = " + str(sleepy_success_accumulator / sleepy_success_counter))
+    result_file.write ("MEAN NONSLEEPY SUCCESS RATE = " + str(nonsleepy_success_accumulator / nonsleepy_success_counter))
+    result_file.write ("\n" + "MEAN SLEEPY SUCCESS RATE = " + str(sleepy_success_accumulator / sleepy_success_counter))
+    result_file.write("\n" + "MEAN TPR = " + str(true_positive_accumulator / true_positive_counter))
+    result_file.write("\n" + "MEAN FPR = " + str(false_positive_accumulator / false_positive_counter))
+    result_file.write("\n" + "MEAN TNR = " + str(true_negative_accumulator / true_negative_counter))
+    result_file.write("\n" + "MEAN FNR = " + str(false_negative_accumulator / false_negative_counter))
 
+    print ("MEAN NONSLEEPY SUCCESS RATE = " + str(nonsleepy_success_accumulator / nonsleepy_success_counter))
+    print ("MEAN SLEEPY SUCCESS RATE = " + str(sleepy_success_accumulator / sleepy_success_counter))
+    print ("MEAN TPR = " + str(true_positive_accumulator / true_positive_counter))
+    print ("MEAN FPR = " + str(false_positive_accumulator / false_positive_counter))
+    print ("MEAN TNR = " + str(true_negative_accumulator / true_negative_counter))
+    print ("MEAN FNR = " + str(false_negative_accumulator / false_negative_counter))
     result_file.close()
 evaluate_everything('C:\\Users\\Mert\\Dropbox\\ITU\\2017 BITIRME\\DATASETS\\COMPOUND_DATASET')
